f5cb9227f4ab57c367a3789376523516d04756bf
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-07-09 14:41:57 +0530

    Refactored the CSS and made the home page responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-export default function Header(){
-    return(
-        <nav className="header">
-            <h1><Link to ="/">Smart<br/>Farming</Link></h1>
-            <main className="header-navs">
-               <Link to ="/">Home</Link>
-               <Link to ="/community">Community</Link>
-               <Link to ="/about">About</Link>
-               <Link to ="/contact">Contact</Link>
-            </main>
-        </nav>
-    )
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <nav className="header">
+      <h1 className="header-title">
+        <Link to="/" className="header-link">
+          Smart
+          <br />
+          Farming
+        </Link>
+      </h1>
+      <div className="hamburger">
+        <FaBars size={30} />
+      </div>
+      <div className="header-navs">
+        <Link to="/" className="header-nav">
+          Home
+        </Link>
+        <Link to="/community" className="header-nav">
+          Community
+        </Link>
+        <Link to="/about" className="header-nav">
+          About
+        </Link>
+        <Link to="/contact" className="header-nav">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  );
 }
